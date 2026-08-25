8125ddea6343163a8b3bb153f4313d438117a5e7
tanishsami <225133498+tans-01@users.noreply.github.com>
2026-08-25 16:53:27 +1000

    added tokentypes and made a scanner class, made few cases for the lexemes

---
+++ Scanner.java
@@ -0,0 +1,52 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+
+    }
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+    public List<Token> scanTokens() {
+        while(!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+   
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+        case '(': addToken(TokenType.LEFT_PAREN); break;
+        case ')': addToken(TokenType.RIGHT_PAREN); break;
+        case '{': addToken(TokenType.LEFT_BRACE); break;
+        case '}': addToken(TokenType.RIGHT_BRACE); break;
+        case ',': addToken(TokenType.COMMA); break;
+        case '.': addToken(TokenType.DOT); break;
+        case '-': addToken(TokenType.MINUS); break;
+        case '+': addToken(TokenType.PLUS); break;
+        case ';': addToken(TokenType.SEMICOLON); break;
+        case '*': addToken(TokenType.STAR); break;
+        }
+    }
+    private char advance() {
+        return source.charAt(current++);
+    }
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start,current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
