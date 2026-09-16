ab4d9b9a579fe30f5238fc41b8bee068ababd08b
tanishsami <225133498+tans-01@users.noreply.github.com>
2026-09-17 08:20:06 +1000

    did the visitor solution to address each class without touching them

--- GenerateAst.java
+++ GenerateAst.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,17 +24,20 @@ public class GenerateAst {
 }
 private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
     String path = outputDir + "/" + baseName + ".java";
-    PrintWriter writer = new PrintWriter(path);
+    PrintWriter writer = new PrintWriter(path, "UTF-8");
 
     writer.println("import java.util.List;");
     writer.println();
     writer.println("abstract class " + baseName + " {");
-
+    defineVisitor(writer, baseName, types);
+    
     for (String type : types) {
         String className = type.split(":")[0].trim();
         String fields = type.split(":")[1].trim();
         defineType(writer, baseName, className, fields);
     }
+     writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
     writer.println("}");
 
     writer.close();
@@ -50,10 +54,31 @@ private static void defineType(PrintWriter writer, String baseName, String class
     writer.println("    }");
 
     writer.println();
+
+    writer.println();                                                                         
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
+    writer.println("    }");
+
     for (String field : fields) {
         writer.println("    final " + field + ";");
     }
 
+   
     writer.println("  }");
 }
+
+private static void defineVisitor (PrintWriter writer, String baseName, List<String>types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+    String typeName = type.split(":")[0].trim();
+        writer.println("    R visit" + typeName + baseName + "(" +
+            typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
+    }
+
 }
